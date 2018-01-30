2b22a572bee5edb89b8744e1642e571fe2e4ec27
Steven Chen <jeepchenup@foxmail.com>
2018-01-30 17:32:18 +0800

    day1

--- python/day1.py
+++ python/day1.py
@@ -293,3 +293,23 @@ def calc(*numbers):
         sum = sum + power(num)
     return sum
 print(calc(1,2))
+
+def person(name, age, **kw):
+    print('name:', name, "age:", age, "other:", kw)
+
+person('Michael', 30)
+person('Bob', 35, city='Beijing')
+person('Adam', 45, gender='M', job='Engineer')
+
+extra = {'city':'Beijing', 'job':'Engineer'}
+person('Jack', 24, city=extra['city'], job=extra['job'])
+person('Jack', 24, **extra)
+
+# 练习
+def product(*args):
+    sum = 1
+    for arg in args:
+        sum = sum * arg
+    return sum
+
+print(product(1,2,3,4,5,6))
